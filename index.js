540713dad671340334ffc583be8b3766fcf51618
Aminul Haque Rana <aminulhaquerana1@gmail.com>
2025-02-12 17:38:21 +0600

    build in module

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const { a, add } = require("./local-1"); //a,add get bygit destractureing
-console.log(a, add(2, 3));
+//local module
+const { a, add } = require("./local-1"); //a,add get by Destructuring
+const { a: a2, add: add2 } = require("./local-2"); //a,add get by Destructuring
+console.log(a2, add2(2, 3, 20));
+
+//build in module
+const path = require("path");
+console.log(path.parse("/C:/Projects/Learing-Node/index.js"));
